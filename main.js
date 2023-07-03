2d352915eb0660c7404bedae4ca677f8b6528ec3
Gabriel Souza <imgabriel.souza071@gmail.com>
2023-07-03 20:09:01 -0300

    update.

---
+++ main.js
@@ -0,0 +1,49 @@
+let lyric = "I want to hold your h";
+
+ for (let i = 0; i < 6; i++) {
+ lyric += "a";
+}
+
+lyric += "nd";
+
+console.log(lyric);
+
+/* ------------------------------------- */
+
+const notifications = 4;
+for (let i = 0; i < notifications; i++) {
+ console.log("You've got mail!");
+}
+
+/* ------------------------------------- */
+
+let guest = 3;
+for (let i = 0; i < guest; i++) {
+  console.log(i);
+}
+
+/* ------------------------------------- */
+
+console.log("*");
+let line = "";
+for (let i = 0 ; i < 5; i++) {
+ line += "~";
+ console.log(line);
+}
+
+/* ------------------------------------- */
+
+console.log("- -");
+console.log(". .");
+let nose = "  >";
+for (let i = 0; i < 3; i++) {
+ nose += "-";
+}
+console.log(nose);
+console.log(" o ");
+
+/* ------------------------------------- */ 
+
+for (let i = 1; i < 5; i++) {
+    console.log("Display Element");
+   }
